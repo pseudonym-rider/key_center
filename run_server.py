01575098528cea3d20bb6803fb8d3a20a2da0368
o-seonsik <dhtjstlr777@gmail.com>
2020-12-05 17:15:15 +0900

    Code refactoring

--- run_server.py
+++ run_server.py
@@ -4,23 +4,27 @@ from server import Server
 app = Flask(__name__)
 server = Server()
 
+
 @app.route('/issue-key', methods=['POST'])
 def issue_key():
     req = request.get_json()
-    response = server.issue_key(req["user_id"], req["type"])
+    response = server.issue_key(req['user_id'], req['type'])
     return jsonify(response)
+
 
 @app.route('/get-sign', methods=['POST'])
 def get_sign():
     req = request.get_json()
-    response = server.sign_msg(req["body"], req["usk"], req["type"])
+    response = server.sign_msg(req['body'], req['usk'], req['type'])
     return jsonify(response)
+
 
 @app.route('/find-signatory', methods=['POST'])
 def open_sign():
     req = request.get_json()
-    response = server.open_sign(req["sign"], req["type"])
+    response = server.open_sign(req['sign'], req['type'])
     return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='80')
