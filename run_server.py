15151ea9be6fa76a3139d8695880b5118774cb52
o-seonsik <dhtjstlr777@gmail.com>
2020-12-12 22:02:07 +0900

    Modify signature logic

--- run_server.py
+++ run_server.py
@@ -1,29 +1,97 @@
 from flask import Flask, jsonify, request
 from server import Server
+from pymongo import MongoClient
+from datetime import datetime
+import config
 
 app = Flask(__name__)
 server = Server()
+client = MongoClient("172.17.0.3")
+qr_db = client.qr_db
+user_to_store_collection = qr_db.user_to_store
+store_to_user_collection = qr_db.store_to_user
 
 
-@app.route('/issue-key', methods=['POST'])
+@app.route('/request/issue-key', methods=['POST'])
 def issue_key():
     req = request.get_json()
-    response = server.issue_key(req['user_id'], req['type'])
+    response = server.issue_key(req["id"], req["type"])
     return jsonify(response)
 
 
-@app.route('/get-sign', methods=['POST'])
-def get_sign():
-    req = request.get_json()
-    response = server.sign_msg(req['body'], req['usk'], req['type'])
-    return jsonify(response)
+# @app.route('/request/get-sign', methods=['POST'])
+# def get_sign():
+#     req = request.get_json()
+#     response = server.sign_msg(req["body"], req["usk"], req["type"])
+#     return jsonify(response)
 
-
-@app.route('/find-signatory', methods=['POST'])
+@app.route('/request/find-signatory', methods=['POST'])
 def open_sign():
     req = request.get_json()
-    response = server.open_sign(req['sign'], req['type'])
+    response = server.open_sign(req["sign"], req["type"])
     return jsonify(response)
+
+
+@app.route('/receive-qr', methods=['POST'])
+def receive_qr():
+    req = request.get_json()
+
+    user_id = req["user-id"]
+    store_id = req["store-id"]
+    qr_time = req["time"]
+    user_secret = req["user-secret"]
+    store_secret = req["store-secret"]
+
+    now_time = (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
+
+    # check validation of time
+    if now_time - int(qr_time) >= 15:
+        return jsonify({"response": False})
+
+    user_data = user_id + "?" + qr_time
+    store_data = store_id + "?" + qr_time
+
+    # user-data and store-data contain each id and qr-time
+    signs_of_user_data = server.sign_msg(user_data, user_secret, store_secret)
+    signs_of_store_data = server.sign_msg(store_data, user_secret, store_secret)
+
+    # save qr code to db
+    doc = {
+        "user-id": user_id,
+        "time": qr_time,
+        "user-sign": signs_of_user_data["user-sign"],
+        "store-sign": signs_of_user_data["store-sign"]
+    }
+    user_to_store_collection.insert(doc)
+    doc = {
+        "store-id": store_id,
+        "time": qr_time,
+        "user-sign": signs_of_store_data["user-sign"],
+        "store-sign": signs_of_store_data["store-sign"]
+    }
+    store_to_user_collection.insert(doc)
+
+    return jsonify({"response": True})
+
+
+# check for the succesful update
+@app.route('/check-mongo', methods=['POST'])
+def check_mongo():
+    req = request.get_json()
+
+    user_id = req["user-id"]
+    store_id = req["store-id"]
+
+    u2s_dict = user_to_store_collection.find({"user-id": user_id})
+    s2u_dict = store_to_user_collection.find({"store-id": store_id})
+
+    print("u2s_dict =>")
+    for x in u2s_dict:
+        print(x)
+    print("s2u_dict =>")
+    for x in s2u_dict:
+        print(x)
+    return jsonify({"u2s_dict": u2s_dict, "s2u_dict": s2u_dict})
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import threading
 class Server:
     _TYPE_USER = "1"
     _TYPE_STORE = "2"
-    
+
     # init function
     def __init__(self):
         self._user_gpk = None
@@ -16,33 +16,33 @@ class Server:
         self._user_msk = None
         self._my_user_gml_id_to_idx = {}
         self._my_user_gml_idx_to_id = {}
-        
+
         self._store_gpk = None
         self._store_gml = None
         self._store_msk = None
         self._my_store_gml_id_to_idx = {}
         self._my_store_gml_idx_to_id = {}
-        
+
         self.setup()
-    
+
     # set keys
     def setup(self):
         groupsig.init(constants.BBS04_CODE, 0)
         bbs04_user = groupsig.setup(constants.BBS04_CODE)
         bbs04_store = groupsig.setup(constants.BBS04_CODE)
-        
+
         self._user_gpk = bbs04_user["grpkey"]
         self._user_gml = bbs04_user["gml"]
         self._user_msk = bbs04_user["mgrkey"]
         self._store_gpk = bbs04_store["grpkey"]
         self._store_gml = bbs04_store["gml"]
         self._store_msk = bbs04_store["mgrkey"]
-    
+
     # add member to my gml
     def update_my_gml(self, id, usk, group_type):
         grpkey, mgrkey, gml, my_id_to_idx, my_idx_to_id \
             = None, None, None, None, None
-        
+
         if group_type == Server._TYPE_USER:
             grpkey = self._user_gpk
             mgrkey = self._user_msk
@@ -55,51 +55,65 @@ class Server:
             gml = self._store_gml
             my_id_to_idx = self._my_store_gml_id_to_idx
             my_idx_to_id = self._my_store_gml_idx_to_id
-        
+
         sign = groupsig.sign("valid", usk, grpkey)
         member = groupsig.open(sign, mgrkey, grpkey, gml)
         mem_id = identity.identity_to_string(member)
-        
+
         my_idx_to_id[mem_id] = id
         if my_id_to_idx.get(id) == None:
             my_id_to_idx[id] = list(mem_id)
         else:
             my_id_to_idx[id].append(mem_id)
-    
+
     # add member and issue user secret key
     def issue_key(self, id, group_type):
         usk = None
         if group_type == Server._TYPE_USER:
-            msg1 = groupsig.join_mgr(0, self._user_msk, self._user_gpk, 
-                                    gml=self._user_gml)
+            msg1 = groupsig.join_mgr(0, self._user_msk, self._user_gpk,
+                                     gml=self._user_gml)
             msg2 = groupsig.join_mem(1, self._user_gpk, msg1)
             usk = msg2["memkey"]
         if group_type == Server._TYPE_STORE:
-            msg1 = groupsig.join_mgr(0, self._store_msk, self._store_gpk, 
-                                    gml=self._store_gml)
+            msg1 = groupsig.join_mgr(0, self._store_msk, self._store_gpk,
+                                     gml=self._store_gml)
             msg2 = groupsig.join_mem(1, self._store_gpk, msg1)
             usk = msg2["memkey"]
-        
+
         t = threading.Thread(target=self.update_my_gml, args=(id, usk, group_type))
         t.start()
-        
+
         base64_gpk = grpkey.grpkey_export(self._user_gpk)
         base64_usk = memkey.memkey_export(usk)
-        return {"gpk":base64_gpk, "usk":base64_usk}
-    
+        return {"gpk": base64_gpk, "usk": base64_usk}
+
     # sign message and return sign
-    def sign_msg(self, body, base64_usk, group_type):
-        grpkey = self._user_gpk if group_type == Server._TYPE_USER else self._store_gpk
-        usk = memkey.memkey_import(constants.BBS04_CODE, base64_usk)
-        
-        sign = groupsig.sign(body, usk, grpkey)
-        base64_sign = signature.signature_export(sign)
-        return {"sign":base64_sign}
-    
+    # def sign_msg(self, body, base64_usk, group_type):
+    #     grpkey = self._user_gpk if group_type == Server._TYPE_USER else self._store_gpk
+    #     usk = memkey.memkey_import(constants.BBS04_CODE, base64_usk)
+    #     sign = groupsig.sign(body, usk, grpkey)
+    #     base64_sign = signature.signature_export(sign)
+    #     return {"sign":base64_sign}
+
+    # sign message and return sign
+    def sign_msg(self, body, base64_user_secret, base64_store_secret):
+        user_grpkey = self._user_gpk
+        store_grpkey = self._store_gpk
+        user_secret = memkey.memkey_import(constants.BBS04_CODE, base64_user_secret)
+        store_secret = memkey.memkey_import(constants.BBS04_CODE, base64_store_secret)
+
+        user_sign = groupsig.sign(body, user_secret, user_grpkey)
+        store_sign = groupsig.sign(body, store_secret, store_grpkey)
+
+        base64_user_sign = signature.signature_export(user_sign)
+        base64_store_sign = signature.signature_export(store_sign)
+
+        return {"user-sign": base64_user_sign, "store-sign": base64_store_sign}
+
     # open message and return signatory's id
     def open_sign(self, base64_sign, group_type):
         mgrkey, grpkey, gml, my_idx_to_id = None, None, None, None
-        
+
         if group_type == Server._TYPE_USER:
             mgrkey = self._user_msk
             grpkey = self._user_gpk
@@ -110,10 +124,10 @@ class Server:
             grpkey = self._store_gpk
             gml = self._store_gml
             my_idx_to_id = self._my_store_gml_idx_to_id
-        
+
         sign = signature.signature_import(constants.BBS04_CODE, base64_sign)
         member = groupsig.open(sign, mgrkey, grpkey, gml)
         mem_id = identity.identity_to_string(member)
         user_id = my_idx_to_id[mem_id]
-        
-        return {"uid":user_id}
+
+        return {"uid": user_id}
